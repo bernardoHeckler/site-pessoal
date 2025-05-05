68402b61c9c3dbc774cdcefef12f0b72c2422dca
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-05-05 11:31:39 -0300

    feat: novo post no blog.

--- src/data/BlogData.jsx
+++ src/data/BlogData.jsx
@@ -1,11 +1,20 @@
 import blogSiteImg from "../imgsitenovo.svg";
 import blogReactImg from "../imgblogreactpost.svg";
+import blogDesign from "../imgDesign.svg";
 
 const BlogData = {
     titulo: "Blog",
     posts: [
       {
         id: 1,
+        imagem: blogDesign,
+        categoria: "Criando Interfaces",
+        data: "05/05/2025",
+        titulo: "Telas de Baixa Fidelidade",
+        descricao: "Estou criando alguns designs de baixa fidelidade para projetos acadêmicos.",
+      },
+      {
+        id: 2,
         imagem: blogSiteImg,
         categoria: "Boas Vindas!",
         data: "02/05/2025",
@@ -13,7 +22,7 @@ const BlogData = {
         descricao: "Fiz este site com muito cuidado e carinho. Foi um projeto que me fez aprender muito e me ajudou a me desenvolver como profissional.",
       },
       {
-        id: 2,
+        id: 3,
         imagem: blogReactImg,
         categoria: "Curtindo aprender",
         data: "01/03/2025",
